463cdf42ee222f89beb6c08100e228f2b4680357
Lisa Wagner <lisawagner.online@gmail.com>
2022-04-28 13:36:54 -0700

    testing out navbar concepts

--- src/components/navbar/Navbar2.jsx
+++ src/components/navbar/Navbar2.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './nav.css'
 
 import {
   Nav,
@@ -13,33 +14,22 @@ import {
 const Navbar2 = () => {
   return (
     <>
-    {/* <div className="topnav">
-      <div className="leftside">
-        <div>Language toggle</div>
-        <div>searchbar</div>
-      </div>
-      <div className="middle">
-        <div>PHENOMINA.</div>
-      </div>
-      <div className="rightside">
-        <div>register/login</div>
-        <div>favourites</div>
-        <div>cart</div>
-      </div>    
-    </div> */}
-    <Bars />
-    <Nav>
-      
-      <NavMenu className="bottomnav">
-        {/* these are product categories, logic to add later */}
-        <NavLink to="/product" activeStyle>essentials</NavLink>
-        <NavLink to="/product" activeStyle>designer</NavLink>
-        <NavLink to="/product" activeStyle>fancy</NavLink>
-        <NavLink to="/product" activeStyle>activewear</NavLink>
-        <NavLink to="/product" activeStyle>accessories</NavLink>
-      </NavMenu>
-    </Nav>
+    <div className="navcontainer">
 
+
+      {/* <Bars /> */}
+      <Nav>
+        
+        <NavMenu className="bottomnav">
+          {/* these are product categories, logic to add later */}
+          <NavLink to="/product" activeStyle>essentials</NavLink>
+          <NavLink to="/product" activeStyle>designer</NavLink>
+          <NavLink to="/product" activeStyle>flaire</NavLink>
+          <NavLink to="/product" activeStyle>activewear</NavLink>
+          <NavLink to="/product" activeStyle>accessories</NavLink>
+        </NavMenu>
+      </Nav>
+    </div>
     </>
   )
 }
